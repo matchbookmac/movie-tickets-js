2e9f3672eeaba80a75548aa9fbc0682c34f94723
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-06-02 14:18:28 -0700

    Add ticket object with properties and method for price.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,27 @@ $( document ).ready(function() {
 });
 
 //raw js
-var foo = function(bar) {
-  return false;
-};
+function Ticket(name, time, age) {
+  this.name = name;
+  this.time = time;
+  this.age = age;
+}
+
+Ticket.prototype.price = function() {
+  var name = this.name;
+  var hours = this.time.getHours();
+  var age = this.age;
+  var price = 15;
+
+  if (age < 16) {
+    price -= 5;
+  } else if (age > 56) {
+    price -= 7;
+  }
+
+  if (hours < 18) {
+    price /= 2;
+  }
+
+  return price;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,21 @@
-describe('foo', function() {
-  it("is some function or expectation", function() {
-    expect(foo('bar')).to.equal(false);
+describe('Ticket', function() {
+
+  describe('new', function() {
+    it("creates a new ticket object", function() {
+      var ticket = new Ticket('Mad Max', new Date(2015, 1, 1, 16, 0), 23);
+      expect(ticket.name).to.equal('Mad Max');
+      expect(ticket.time.getDate()).to.equal(1);
+      expect(ticket.time.getHours()).to.equal(16);
+      expect(ticket.time.getFullYear()).to.equal(2015);
+      expect(ticket.age).to.equal(23);
+    });
+  })
+
+  describe('price', function() {
+    it("returns the price of the ticket based on its parameters", function() {
+      var ticket = new Ticket('Mad Max', new Date(2015, 1, 1, 16, 0), 23);
+      expect(ticket.price()).to.equal(7.5);
+    });
   });
+
 });
