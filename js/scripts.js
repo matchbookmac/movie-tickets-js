1e7ddec414a1c326ffd08b5a0e5c28db07c03fca
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-06-02 16:21:25 -0700

    Add sliding price well.

--- js/scripts.js
+++ js/scripts.js
@@ -26,12 +26,40 @@ $( document ).ready(function() {
     var time = $("input#time").val().split(':');
     var age = $("select#age").val();
     if(title.trim() != "" && age != "Select an Age") {
-      $("#ticket-price").show();
+      $("#ticket-price").slideToggle("slow");
+      $("#no-button").hide();
+      $("#go-button").show();
       var ticket = new Ticket(title, new Date(2015, 1, 1, parseInt(time[0]), parseInt(time[1])), parseInt(age));
       $("#ticket-price").html("Cost: $" + ticket.price());
     } else {
-      $("#ticket-price").hide();
+      $("#ticket-price").slideToggle("slow");
+      $("#go-button").hide();
+      $("#no-button").show();
     }
+  }
+
+
+  $("form#buy-ticket").submit(function(event) {
+    event.preventDefault();
+
+    var title = $("input#title").val();
+    var time = $("input#time").val().split(':');
+    var age = $("select#age").val();
+    if(title.trim() != "" && age != "Select an Age") {
+      var ticket = new Ticket(title, new Date(2015, 1, 1, parseInt(time[0]), parseInt(time[1])), parseInt(age));
+      $("table#purchased-tickets").append('<tr>' + '<td>' + title + '</td>' + '<td>' + time.join(":") + '</td>' + '<td>$' + ticket.price() + '</td>' + '</tr>');
+      clearForms();
+      updatePrice();
+      $("#purchased-tickets-panel").show();
+    } else {
+
+    }
+  });
+
+  function clearForms() {
+    $("input#title").val("");
+    $("input#time").val("09:30");
+    $("input#age").val("Select an Age");
   }
 
 });
