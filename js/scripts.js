c097e62df6274bdca158970f6c820af7d4281d4a
Ian MacDonald & Toby Alden <student@epicodus.com>
2015-06-02 15:46:27 -0700

    Add auto-updating price well.

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,48 @@
 
 //jQuery
 $( document ).ready(function() {
-    console.log( "jQuery Ready" );
 
-    $("#jqtest").text('jQuery Ready')
+  $('.clockpicker').clockpicker();
+
+  var select = '';
+  select += '<option>Select an Age</option>';
+  for (var i = 1; i <= 100; i++) {
+      select += '<option val=' + i + '>' + i + '</option>';
+  }
+  $('select#age').html(select);
+
+  $('input').change(function() {
+    updatePrice();
+  });
+
+  $('select').change(function() {
+    updatePrice();
+  });
+
+  function updatePrice() {
+    var title = $("input#title").val();
+    var time = $("input#time").val().split(':');
+    var age = $("select#age").val();
+    if(title.trim() != "" && age != "Select an Age") {
+      $("#ticket-price").show();
+      var ticket = new Ticket(title, new Date(2015, 1, 1, parseInt(time[0]), parseInt(time[1])), parseInt(age));
+      $("#ticket-price").html("Cost: $" + ticket.price());
+    } else {
+      $("#ticket-price").hide();
+    }
+  }
+
 });
 
 //raw js
-function Ticket(name, time, age) {
-  this.name = name;
+function Ticket(title, time, age) {
+  this.title = title;
   this.time = time;
   this.age = age;
 }
 
 Ticket.prototype.price = function() {
-  var name = this.name;
+  var title = this.title;
   var hours = this.time.getHours();
   var age = this.age;
   var price = 15;
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,7 @@ describe('Ticket', function() {
   describe('new', function() {
     it("creates a new ticket object", function() {
       var ticket = new Ticket('Mad Max', new Date(2015, 1, 1, 16, 0), 23);
-      expect(ticket.name).to.equal('Mad Max');
+      expect(ticket.title).to.equal('Mad Max');
       expect(ticket.time.getDate()).to.equal(1);
       expect(ticket.time.getHours()).to.equal(16);
       expect(ticket.time.getFullYear()).to.equal(2015);
